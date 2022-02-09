113309e64e1a2aa2c7b78ccdfd7f259a956695d4
SimonT <simon@tunstig.se>
2022-02-09 11:50:13 +0100

    RestController funkar

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO Booking (CustomerId, EmployeeId, BookingDate, StartTime, EndTime)
 
-VALUES (1, 1, '2018-03-08', '12:00', '13:00');
-VALUES (1, 1, '2018-03-03', '12:00', '13:00');
+VALUES (1, 1, '2018-03-08', '12:00', '13:00'),
+ (1, 1, '2018-03-03', '12:00', '13:00');
 
