a3d09da3030e6c73a6454eadd716390dda47fcb0
SimonT <simon@tunstig.se>
2022-02-15 11:49:10 +0100

    Fixat kalendern.

--- src/main/resources/static/javaScript.js
+++ src/main/resources/static/javaScript.js
@@ -12,14 +12,14 @@ function renderDate() {
    let endDate = new Date(dt.getFullYear(), dt.getMonth() + 1,0).getDate();
    let prevDate = new Date(dt.getFullYear(), dt.getMonth(), 0).getDate();
 
-   let months = ["January", "February", "March", "April", "May", "June", "July",
-               "August", "September", "October", "November","December"]
+   let months = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli",
+                  "Augusti", "September", "Oktober", "November", "December"]
 
    document.getElementById("month").innerHTML = months[dt.getMonth()];
    document.getElementById("date_str").innerHTML = dt.toDateString();
 
    let cells = "";
-   for (x = day; x > 0; x--) {
+   for (x = day; x > 1; x--) {
        cells += "<div class='prev_date'>" + (prevDate - x + 1) + "</div>";
    }
 
