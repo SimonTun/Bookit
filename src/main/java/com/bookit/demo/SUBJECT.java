fe1d137ce24a9003e713e2dad737a267720e63a3
Adam <a.bengtsson1988@gmail.com>
2022-02-16 13:10:41 +0100

    Enums rätt + ny css

--- src/main/java/com/bookit/demo/SUBJECT.java
+++ src/main/java/com/bookit/demo/SUBJECT.java
@@ -1,7 +1,20 @@
 package com.bookit.demo;
 
 public enum SUBJECT {
-    PENSIONSAVINGS,CAPITALSAVINGS,MORTAGES,INSURANCE,CHILDSAVINGS;
+    PENSIONSAVINGS ("Pensionsparande"),
+    CAPITALSAVINGS ("Kapitalplacering"),
+    MORTAGES ("Bolån"),
+    INSURANCE ("Försäkring"),
+    CHILDSAVINGS ("Barnsparande");
 
+    private final String displayValue;
 
+    private SUBJECT (String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
+
